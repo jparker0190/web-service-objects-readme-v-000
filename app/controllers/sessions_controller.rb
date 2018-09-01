9f4953c82314495772004c24468ac4a4d0d5c55e
Joseph Parker <jparker010190@gmail.com>
2018-09-01 22:28:20 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,6 @@ class SessionsController < ApplicationController
     foursquare = FoursquareService.new
     session[:token] = foursquare.authenticate!
     (ENV['FOURSQUARE_CLIENT_ID'], ENV['FOURSQUARE_SECRET'], params[:code])
-    redirect_to root_pah
+    redirect_to root_path
   end
 end
